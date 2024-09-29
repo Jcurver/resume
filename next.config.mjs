1a4b08ff1d17775cd295a07eb2cb5a25464b3992
ifizzyou <pjh15237213@gmail.com>
2024-09-29 13:06:41 +0900

    first4

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
+const prefix = process.env.NODE_ENV === "production" ? "https://yeji-j.github.io/test/" : "";
+
 const nextConfig = {
   output: "export",
+  assetPrefix: prefix,
 };
 
 export default nextConfig;
